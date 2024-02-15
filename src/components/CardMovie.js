d8107356d829f9e210704261c2c54245f1bc223e
Omar-shaqra <49815667+Omar-shaqra@users.noreply.github.com>
2024-02-15 21:45:47 +0200

    Create CardMovie.js

---
+++ src/components/CardMovie.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const CardMovie = ({ mov }) => {
+  return (
+    <Col xs="6" sm="6" md="4" lg="3" className="my-1">
+      <Link to={`/movie/${mov.id}`}>
+        <div className="card">
+          <img
+            src={`https://images.tmdb.org/t/p/w500` + mov.poster_path}
+            className="card__image"
+            alt="hu"
+          />
+          <div className="card__overlay">
+            <div className="overlay__text text-center w-100 p-2">
+              <p>movie name : {mov.title} </p>
+              <p>release_date :{mov.release_date}</p>
+              <p>vote_count : {mov.vote_count}</p>
+              <p>Rate : {mov.vote_average}</p>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </Col>
+  );
+};
+
+export default CardMovie;
