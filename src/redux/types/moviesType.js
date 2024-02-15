6fd10efe188fbd684e58c45e8ea6a52a6ba9a722
Omar-shaqra <49815667+Omar-shaqra@users.noreply.github.com>
2024-02-15 21:46:31 +0200

    Create moviesType.js

---
+++ src/redux/types/moviesType.js
@@ -0,0 +1,3 @@
+export const Allmovies = "allmovies";
+export const movieApi =
+  "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=63e13b4faddf2c10b7298cf7b76f0fc4";
